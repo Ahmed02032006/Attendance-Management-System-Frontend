d9f95796779b6f642ec13e4208e4871e8de9b271
Ahmed02032006 <m.ahmedofficial677@gmail.com>
2026-02-04 17:30:43 +0500

    sorting issues for admin scenarion in admin panel

--- src/page/Admin_Pages/AdminTeachers_Page.jsx
+++ src/page/Admin_Pages/AdminTeachers_Page.jsx
@@ -398,8 +398,8 @@ const AdminTeachers_Page = () => {
                           </td>
                           <td className="px-4 py-4 whitespace-nowrap text-center">
                             <span className={`px-1 py-1 text-xs font-semibold rounded-full flex items-center justify-center w-[75px] mx-auto ${teacher.status === "Active"
-                              ? "bg-green-50 text-green-700 border border-green-200"
-                              : "bg-red-50 text-red-700 border border-red-200"
+                              ? "bg-green-50 text-green-800 border border-green-200"
+                              : "bg-red-50 text-red-800 border border-red-200"
                               }`}>
                               {teacher.status === "Active" ? (
                                 <>
