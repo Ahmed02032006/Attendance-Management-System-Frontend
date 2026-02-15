56d8196d7e3211965cb439f139e9ff8918baa996
Ahmed02032006 <m.ahmedofficial677@gmail.com>
2026-02-15 19:59:10 +0500

    changing font for logo

--- src/layout/Teacher_Dashboard_Layout/TeacherDashboardLayout.jsx
+++ src/layout/Teacher_Dashboard_Layout/TeacherDashboardLayout.jsx
@@ -264,7 +264,7 @@ const TeacherDashboardLayout = () => {
                 sidebarOpen ? (
                   <>
                     <div className='flex items-center gap-4'>
-                      <img src="https://www.shutterstock.com/image-vector/effective-teamwork-icon-black-thin-600nw-2265229773.jpg" width={"40px"} height={"25px"} />
+                      <img src="client/public/Side-Logo/1.png" width={"40px"} height={"25px"} />
                       <p className='text-2xl orbitron'>ATTENDLY</p>
                     </div>
                   </>
