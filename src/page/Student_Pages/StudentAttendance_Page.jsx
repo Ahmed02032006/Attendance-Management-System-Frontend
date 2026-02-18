9ae198d55bc6ea2604f94e49b87ab025314d5fa3
Ahmed02032006 <m.ahmedofficial677@gmail.com>
2026-02-18 16:11:53 +0500

    solving issues

--- src/page/Student_Pages/StudentAttendance_Page.jsx
+++ src/page/Student_Pages/StudentAttendance_Page.jsx
@@ -133,7 +133,7 @@ const StudentAttendance_Page = () => {
       const urlParams = new URLSearchParams(locationHook.search);
       const code = urlParams.get('code');
       const subject = urlParams.get('subject');
-      const subjectName = urlParams.get('subjectName');
+      const departmentOffering = urlParams.get('departmentOffering');
       const expiry = urlParams.get('expiry');
 
       if (code) {
@@ -152,7 +152,7 @@ const StudentAttendance_Page = () => {
         setQrData({
           code,
           subject: subject || 'Unknown Subject',
-          subjectName: subjectName || 'Unknown Subject Name',
+          departmentOffering: departmentOffering || 'Unknown Program Name',
           type: 'attendance',
           expiryTimestamp: expiry ? new Date(parseInt(expiry)).toISOString() : null
         });
@@ -294,7 +294,7 @@ const StudentAttendance_Page = () => {
       const AttendanceData = {
         ...formData,
         uniqueCode: originalCode, // Send original code to backend
-        subjectName: qrData.subjectName,
+        departmentOffering: qrData.departmentOffering,
         subjectId: qrData.subject,
         time: currentTime,
         date: qrData.attendanceDate,
@@ -460,7 +460,7 @@ const StudentAttendance_Page = () => {
                 <h2 className="text-xl font-semibold text-gray-800">Mark Your Attendance</h2>
                 {qrData && (
                   <div className="mt-0.5 text-xs text-gray-600 space-y-1">
-                    <p><span className="font-medium">Subject Name:</span> <span className='border-b border-gray-400'>{qrData.subjectName}</span></p>
+                    <p><span className="font-medium">Department Offering the Course:</span> <span className='border-b border-gray-400'>{qrData.departmentOffering}</span></p>
                     {/* <div className="flex items-center mt-1">
                       <span className="text-xs font-medium px-2 py-0.5 rounded-full bg-green-100 text-green-800 flex items-center">
                         <svg className="w-2 h-2 mr-1" fill="currentColor" viewBox="0 0 20 20">
