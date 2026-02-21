10943b4ce91739a1fb0a3047ee6ad5dba23e41a9
Ahmed02032006 <m.ahmedofficial677@gmail.com>
2026-02-21 19:59:41 +0500

    solving some issue

--- src/page/Teacher_Pages/TeacherAttendance_Page.jsx
+++ src/page/Teacher_Pages/TeacherAttendance_Page.jsx
@@ -1326,7 +1326,7 @@ const TeacherAttendance_Page = () => {
                       </div>
                       <div>
                         <p className="font-medium text-gray-700">Current Subject</p>
-                        <p className="text-gray-900">{selectedStudent.subjectTitle}</p>
+                        <p className="text-gray-900">{selectedStudent.title}</p>
                       </div>
                     </div>
                   </div>
