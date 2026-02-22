fc39e8cbc50245705a45feb5a24f6c7853169655
Ahmed02032006 <m.ahmedofficial677@gmail.com>
2026-02-22 15:55:46 +0500

    changes background image in login page

--- src/layout/Auth_Layout/AuthLayout.jsx
+++ src/layout/Auth_Layout/AuthLayout.jsx
@@ -9,7 +9,7 @@ const AuthLayout = () => {
         backgroundImage: `url('https://attendanceradar.com/wp-content/uploads/2024/06/DALL%C2%B7E-2024-06-28-19.10.18-A-university-classroom-where-a-teacher-is-taking-attendance.-The-teacher-stands-at-the-front-of-the-class-with-a-clipboard-or-tablet-checking-off-nam-1024x585.webp')`,
       }}
     >
-      <div className="w-full max-w-md bg-white bg-opacity-90 backdrop-blur-sm border-gray-400 border-[0.5px] p-1 rounded-xl shadow-lg">
+      <div className="w-full max-w-md bg-white bg-opacity-90 backdrop-blur-lg border-gray-400 border-[0.5px] p-1 rounded-xl shadow-lg">
         <Outlet />
       </div>
     </div>
