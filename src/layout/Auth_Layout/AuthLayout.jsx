6800c50b028d547dcba64316d455b1849ea68a4b
Ahmed02032006 <m.ahmedofficial677@gmail.com>
2026-02-22 15:57:50 +0500

    changes background image in login page

--- src/layout/Auth_Layout/AuthLayout.jsx
+++ src/layout/Auth_Layout/AuthLayout.jsx
@@ -6,10 +6,10 @@ const AuthLayout = () => {
     <div
       className="min-h-screen w-full bg-cover bg-center flex items-center justify-center px-4"
       style={{
-        backgroundImage: `url('https://www.shutterstock.com/image-photo/rear-view-man-raising-arm-600nw-2345710689.jpg')`,
+        backgroundImage: `url('https://cdn.pixabay.com/photo/2022/05/24/04/38/study-7217599_1280.jpg')`,
       }}
     >
-      <div className="w-full max-w-md bg-white bg-opacity-90 backdrop-blur-md border-gray-400 border-[0.5px] p-1 rounded-xl shadow-lg">
+      <div className="w-full max-w-md bg-white bg-opacity-90 backdrop-blur-sm border-gray-400 border-[0.5px] p-1 rounded-xl shadow-lg">
         <Outlet />
       </div>
     </div>
