58405b33b973580f9063754031fc7e233d9f16f8
Ahmed02032006 <m.ahmedofficial677@gmail.com>
2026-02-27 16:07:36 +0500

    changing brand name for attendance management system

--- src/layout/Admin_Dashboard_Layout/AdminDashboardLayout.jsx
+++ src/layout/Admin_Dashboard_Layout/AdminDashboardLayout.jsx
@@ -149,7 +149,7 @@ const AdminDashboardLayout = () => {
                 sidebarOpen ? (
                   <>
                     <div className='flex items-center gap-4 cursor-pointer'>
-                      <img src="/Side-Logo/1.png" width={"35px"} height={"20px"} />
+                      <img src="/Side-Logo/1.png" width={"35px"} height={"18px"} />
                       <p className='text-2xl orbitron'>ATTMARK</p>
                     </div>
                   </>
