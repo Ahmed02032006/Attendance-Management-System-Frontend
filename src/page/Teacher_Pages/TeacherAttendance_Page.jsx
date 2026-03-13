5ceef6259481884636ab5568bb9afc670c543805
Ahmed02032006 <m.ahmedofficial677@gmail.com>
2026-03-13 16:40:33 +0500

    some sorting in teacher pages

--- src/page/Teacher_Pages/TeacherAttendance_Page.jsx
+++ src/page/Teacher_Pages/TeacherAttendance_Page.jsx
@@ -859,7 +859,7 @@ const TeacherAttendance_Page = () => {
                     {/* <svg className="mr-0.5 w-3 h-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
                     </svg> */}
-                    <FiChevronDown className="mr-1 w-5 h-5" />
+                    <FiChevronDown className="mr-1 w-4 h-4" />
                   </button>
                 )}
               </div>
